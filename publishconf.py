e828c2c4ee986f09ebd6ef543c15d85281590957
benhoyle <benjhoyle@gmail.com>
2018-03-23 12:53:21 +0000

    HTTP*S* on site URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://benhoyle.github.io'
+SITEURL = 'https://benhoyle.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
