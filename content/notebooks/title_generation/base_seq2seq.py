d6edbefd657a2a7af7c368d3d116337baa471fab
Ben Hoyle <benjhoyle@gmail.com>
2018-04-17 16:00:34 +0100

    Playing with character decoding

--- content/notebooks/title_generation/base_seq2seq.py
+++ content/notebooks/title_generation/base_seq2seq.py
@@ -8,110 +8,16 @@ from keras.preprocessing import text
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-
 from abstract_model_wrapper import AbstractModelWrapper
 
-
-class BaseSeq2Seq(AbstractModelWrapper):
-    """ Abstract class for sequence to sequence models for mixing in. """
-
-    def __init__(
-        self,
-        encoder_texts=None,
-        decoder_texts=None,
-        encoder_seq_length=None,
-        decoder_seq_length=None,
-        num_encoder_tokens=None,
-        num_decoder_tokens=None,
-        latent_dim=300,
-        weights_file=None,
-        training_set_size=250,  # Due to memory we need to train in sets
-        batch_size=32
-    ):
-        # If encoder / decoder seq_length = none we can set based on data
-        self.encoder_seq_length = encoder_seq_length
-        self.decoder_seq_length = decoder_seq_length
-        # Likewise if the x_tokens are None we can set later based on the data
-        self.num_encoder_tokens = num_encoder_tokens
-        self.num_decoder_tokens = num_decoder_tokens
-        self.latent_dim = latent_dim
-        self.training_set_size = training_set_size
-        self.batch_size = batch_size
-        # If passed with init process texts
-        self.input_tokenizer = None
-        self.output_tokenizer = None
-        self.load_text_data(encoder_texts, decoder_texts)
-        self._start_checks()
-        self.model = None
-        self._build_model()
-        # Build model here?
-        if weights_file and self.model:
-            self.weights_file = weights_file
-
-            self._load_weights()
-        else:
-            self.weights_file = "weights.hdf5"
-        self._reset_metrics()
-
-    # These are the exposed methods (to keep things simple)
-    def load_text_data(self, encoder_texts, decoder_texts):
-        """ Print a representation of the model. """
-        self.encoder_texts = encoder_texts
-        self.decoder_texts = decoder_texts
-
-        # Convert to seqs - this will generate tokenizers
-        self.input_data = self._text2seq(self.encoder_texts, encoder=True)
-        self.output_data = self._text2seq(self.decoder_texts, encoder=False)
-
-        # Set dictionaries - we need dictionaries in each direction
-        self.input_dictionary = self.input_tokenizer.word_index
-        self.output_dictionary = self.output_tokenizer.word_index
-        self.input_dictionary_rev = dict(
-            (i, char) for char, i in self.input_dictionary.items()
-        )
-        self.output_dictionary_rev = dict(
-            (i, char) for char, i in self.output_dictionary.items()
-        )
-
-        o_string = (
-            "Our input data has shape {0} and"
-            " our output data has shape {1}"
-            )
-        print(o_string.format(self.input_data.shape, self.output_data.shape))
-        # Split into test and train
-        self._split_data()
-
-    def predict(self, input_data):
-        """ Predict output text from input text. """
-        # if input is text convert to sequence
-        if isinstance(input_data, str):
-            # If input_text is string then need to convert to single item list
-            input_seq = self._text2seq([input_data], encoder=True)
-        else:
-            input_seq = input_data
-        predicted_output_seq = self._predict_from_seq(input_seq)
-        predicted_text = self._seq2text(predicted_output_seq)
-        return predicted_text
-
-    def train(self, epochs=1, reset_metrics=False):
-        """ Train in batches on all data with validation. """
-        if reset_metrics:
-            self._reset_metrics()
-        for e in range(0, epochs):
-            print("Training for epoch {0}".format(e))
-            self._train_epoch()
-            self._save_weights()
-            self.example_output(5)
-        self.plot_loss()
+class MemEfficientS2S(AbstractModelWrapper):
+    """ General functions for a memory efficient sequence-2-sequence
+    model. """
 
     def print(self):
         """ Print model summary."""
         print("Training Model:\n")
         print(self.model.summary())
-
-    def _save_weights(self):
-        """ Load weights from file. """
-        self.model.save_weights(self.weights_file, overwrite=True)
 
     def plot_loss(self):
         """ Plot training and validation loss. """
@@ -123,48 +29,9 @@ class BaseSeq2Seq(AbstractModelWrapper):
         plt.legend(['train', 'test'], loc='upper left')
         plt.show()
 
-    def example_output(self, number):
-        """ Print a number of example predictions. """
-
-        # Select a set of test data at random
-        num_test_titles = len(self.input_test_data)
-        indices = random.sample(range(0, num_test_titles), number)
-        print("------------------------------------------")
-        for i in indices:
-            input_sample = self.input_test_data[i]
-            output_sample = self.output_test_data[i]
-            predicted_text = self.predict(input_sample)
-            output_sample_text = self._seq2text(output_sample)
-            claim_text = self._seq2text(input_sample, output=False)
-            print("Sample of claim text: {}\n".format(claim_text[0:200]))
-            o_string = (
-                "Predicted title is: {}"
-                " \nActual title is: {} \n---"
-            )
-            print(o_string.format(predicted_text, output_sample_text))
-
-    def _seq2text(self, seq, output=True):
-        """ Convert a sequence of integers to text."""
-        control_ints = [
-            self.output_dictionary["stopseq"],
-            self.output_dictionary["startseq"],
-            0
-        ]
-        text = ''
-        for k in seq:
-            w = ''
-            k = k.astype(int)
-            if output:
-                # Adapted to take account of different control integers
-                if k not in control_ints and k < self.num_decoder_tokens:
-                    w = self.output_dictionary_rev[k]
-            else:
-                # If input
-                if k != 0 and k < self.num_decoder_tokens:
-                    w = self.input_dictionary_rev[k]
-            if w:
-                text = text + w + ' '
-        return text
+    def _save_weights(self):
+        """ Load weights from file. """
+        self.model.save_weights(self.weights_file, overwrite=True)
 
     def _load_weights(self):
         """ Load weights from file. """
@@ -260,6 +127,147 @@ class BaseSeq2Seq(AbstractModelWrapper):
                     )
                 )
             self._train_set(training_data, validation_data)
+
+    def _reset_metrics(self):
+        """ Reset tracked training metrics. """
+        self.train_loss = []
+        self.val_loss = []
+
+
+class BaseSeq2Seq(MemEfficientS2S):
+    """ Abstract class for sequence to sequence models for mixing in. """
+
+    def __init__(
+        self,
+        encoder_texts=None,
+        decoder_texts=None,
+        encoder_seq_length=None,
+        decoder_seq_length=None,
+        num_encoder_tokens=None,
+        num_decoder_tokens=None,
+        latent_dim=300,
+        weights_file=None,
+        training_set_size=250,  # Due to memory we need to train in sets
+        batch_size=32
+    ):
+        # If encoder / decoder seq_length = none we can set based on data
+        self.encoder_seq_length = encoder_seq_length
+        self.decoder_seq_length = decoder_seq_length
+        # Likewise if the x_tokens are None we can set later based on the data
+        self.num_encoder_tokens = num_encoder_tokens
+        self.num_decoder_tokens = num_decoder_tokens
+        self.latent_dim = latent_dim
+        self.training_set_size = training_set_size
+        self.batch_size = batch_size
+        # If passed with init process texts
+        self.input_tokenizer = None
+        self.output_tokenizer = None
+        self.load_text_data(encoder_texts, decoder_texts)
+        self._start_checks()
+        self.model = None
+        self._build_model()
+        # Build model here?
+        if weights_file and self.model:
+            self.weights_file = weights_file
+
+            self._load_weights()
+        else:
+            self.weights_file = "weights.hdf5"
+        self._reset_metrics()
+
+    # These are the exposed methods (to keep things simple)
+    def load_text_data(self, encoder_texts, decoder_texts):
+        """ Print a representation of the model. """
+        self.encoder_texts = encoder_texts
+        self.decoder_texts = decoder_texts
+
+        # Convert to seqs - this will generate tokenizers
+        self.input_data = self._text2seq(self.encoder_texts, encoder=True)
+        self.output_data = self._text2seq(self.decoder_texts, encoder=False)
+
+        # Set dictionaries - we need dictionaries in each direction
+        self.input_dictionary = self.input_tokenizer.word_index
+        self.output_dictionary = self.output_tokenizer.word_index
+        self.input_dictionary_rev = dict(
+            (i, char) for char, i in self.input_dictionary.items()
+        )
+        self.output_dictionary_rev = dict(
+            (i, char) for char, i in self.output_dictionary.items()
+        )
+
+        o_string = (
+            "Our input data has shape {0} and"
+            " our output data has shape {1}"
+            )
+        print(o_string.format(self.input_data.shape, self.output_data.shape))
+        # Split into test and train
+        self._split_data()
+
+    def predict(self, input_data):
+        """ Predict output text from input text. """
+        # if input is text convert to sequence
+        if isinstance(input_data, str):
+            # If input_text is string then need to convert to single item list
+            input_seq = self._text2seq([input_data], encoder=True)
+        else:
+            input_seq = input_data
+        predicted_output_seq = self._predict_from_seq(input_seq)
+        predicted_text = self._seq2text(predicted_output_seq)
+        return predicted_text
+
+    def train(self, epochs=1, reset_metrics=False):
+        """ Train in batches on all data with validation. """
+        if reset_metrics:
+            self._reset_metrics()
+        for e in range(0, epochs):
+            print("Training for epoch {0}".format(e))
+            self._train_epoch()
+            self._save_weights()
+            self.example_output(5)
+        self.plot_loss()
+
+    def example_output(self, number):
+        """ Print a number of example predictions. """
+
+        # Select a set of test data at random
+        num_test_titles = len(self.input_test_data)
+        indices = random.sample(range(0, num_test_titles), number)
+        print("------------------------------------------")
+        for i in indices:
+            input_sample = self.input_test_data[i]
+            output_sample = self.output_test_data[i]
+            predicted_text = self.predict(input_sample)
+            output_sample_text = self._seq2text(output_sample)
+            claim_text = self._seq2text(input_sample, output=False)
+            print("Sample of claim text: {}\n".format(claim_text[0:200]))
+            o_string = (
+                "Predicted title is: {}"
+                " \nActual title is: {} \n---"
+            )
+            print(o_string.format(predicted_text, output_sample_text))
+
+    def _seq2text(self, seq, output=True):
+        """ Convert a sequence of integers to text."""
+        control_ints = [
+            self.output_dictionary["stopseq"],
+            self.output_dictionary["startseq"],
+            0
+        ]
+        text = ''
+        for k in seq:
+            w = ''
+            k = k.astype(int)
+            if output:
+                # Adapted to take account of different control integers
+                if k not in control_ints and k < self.num_decoder_tokens:
+                    w = self.output_dictionary_rev[k]
+            else:
+                # If input
+                if k != 0 and k < self.num_decoder_tokens:
+                    w = self.input_dictionary_rev[k]
+            if w:
+                text = text + w + ' '
+        return text
 
     def _reset_metrics(self):
         """ Reset tracked training metrics. """
